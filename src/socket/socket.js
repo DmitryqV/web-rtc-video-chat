1343b58824964b837d66f6fd0a5b2ceef0ff050e
DmitryqV <vlasenko_dima@icloud.com>
2021-09-23 01:29:23 +0500

    added ui room list

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 
-const socket = io('http://localhost:3001/', {
+const socket = io('/', {
   forceNew: true,
   reconnectionAttempts: "Infinity",
   timeout : 1000, 
