308b8e1d17ce52942e3302dc808cc980cda841c8
DmitryqV <vlasenko_dima@icloud.com>
2021-09-24 02:09:10 +0500

    added server log file

--- server/log/logger.js
+++ server/log/logger.js
@@ -1,4 +1,9 @@
-const logger = require("log4js").getLogger();
-logger.level = 'debug';
+const log4js = require("log4js");
+logger = log4js.getLogger();
+logger.level = 'info';
 
+log4js.configure({
+    appenders: { cheese: { type: "file", filename: "server.log" } },
+    categories: { default: { appenders: ["cheese"], level: "info" } }
+});
 module.exports = logger;
