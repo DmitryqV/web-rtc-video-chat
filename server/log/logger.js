01c095214d0985df3395075710aaed1db1b1641f
DmitryqV <vlasenko_dima@icloud.com>
2021-09-24 02:34:59 +0500

    added editor config and refactoring code base

--- server/log/logger.js
+++ server/log/logger.js
@@ -3,7 +3,8 @@ logger = log4js.getLogger();
 logger.level = 'info';
 
 log4js.configure({
-    appenders: { cheese: { type: "file", filename: "server.log" } },
-    categories: { default: { appenders: ["cheese"], level: "info" } }
+  appenders: { cheese: { type: "file", filename: "server.log" } },
+  categories: { default: { appenders: ["cheese"], level: "info" } }
 });
+
 module.exports = logger;
