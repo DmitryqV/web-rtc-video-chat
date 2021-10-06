fc96faad7d185f0310cac42bd62a3a93741ec0e5
DmitryqV <vlasenko_dima@icloud.com>
2021-10-06 10:07:30 +0500

    type the entire project backend

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const path = require('path');
 const logger = require('./server/log/logger');
 const app = express();
 const server = require('http').createServer(app);
-const port = process.env.PORT || 3333;
+const port = process.env.PORT || 3001;
 
 try {
   require('./server/services/socket-service')(
     require('socket.io')(server)
   );
   logger.info('socket services loaded');
-} catch (e) {
+} catch {
   logger.error('socket services not loaded!');
 }
 
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV === 'production') {
     try {
       req.sendStatus(200).sendFile(path.resolve(__dirname, 'build', 'index.html'));
       logger.info('build succeeded');
-    } catch (e) {
+    } catch {
       logger.error('building error, folder "build" not found in root dirictory.');
       req.sendStatus(404);
     }
