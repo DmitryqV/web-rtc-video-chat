b9bc53548ba988baa3fa25782ceaf607eb19e469
DmitryqV <vlasenko_dima@icloud.com>
2021-10-08 23:35:25 +0500

    fix server side error

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ const path = require('path');
 const logger = require('./server/log/logger');
 const app = express();
 const server = require('http').createServer(app);
-const port: any = process.env.PORT || 3001;
+const port: string | number = process.env.PORT || 3001;
 
 try {
   require('./server/services/socket-service')(
@@ -12,7 +12,6 @@ try {
   logger.info('socket services loaded');
 } catch (e) {
   logger.error('socket services not loaded!', e);
-  console.log(e);
 }
 
 if (process.env.NODE_ENV === 'production') {
@@ -21,8 +20,8 @@ if (process.env.NODE_ENV === 'production') {
     try {
       req.sendStatus(200).sendFile(path.resolve(__dirname, 'build', 'index.html'));
       logger.info('build succeeded');
-    } catch {
-      logger.error('building error, folder "build" not found in root dirictory.');
+    } catch (e) {
+      logger.error('building error, folder "build" not found in root dirictory.', e);
       req.sendStatus(404);
     }
   });
