9389064fab7b8197cdd3abbf5eace4ab6c955f9f
DmitryqV <vlasenko_dima@icloud.com>
2021-10-25 17:10:43 +0500

    change gitignore file and logger js

--- server/log/logger.js
+++ server/log/logger.js
@@ -3,8 +3,13 @@ const logger = log4js.getLogger();
 logger.level = 'info';
 
 log4js.configure({
-  appenders: { cheese: { type: "file", filename: "server.log" } },
-  categories: { default: { appenders: ["cheese"], level: "info" } }
+  appenders: {
+    'console': { type: 'console' },
+    'file': { type: 'file', filename: 'server.log' }
+  },
+  categories: {
+    default: { appenders: ['file', 'console'], level: 'INFO' },
+  }
 });
 
 module.exports = logger;
