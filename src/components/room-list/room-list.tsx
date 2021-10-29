82c2be7dde82834e4eda026e4b0ed66c42890501
DmitryqV <vlasenko_dima@icloud.com>
2021-10-29 15:38:13 +0500

    change room list

--- src/components/room-list/room-list.tsx
+++ src/components/room-list/room-list.tsx
@@ -10,22 +10,24 @@ export const RoomList: FC<IRooms> = ({ rooms }) => {
   return (
     <section className='room-list'>
       <h1 className='room-list__title'> Room list </h1>
-      {
-        rooms !== undefined && rooms.map((roomID: string) => {
-          return (
-            <div className='room-list__item' key={roomID} onClick={() => {
-              history.push('/room/' + roomID);
-            }} title={'/room/' + roomID}>
-              <span className='room-list__item-content'>
-                <div className='room-list__item-avatar'>
-                  {roomID[0]}
-                </div>
-                <p className='room-list__item-title'>{roomID}</p>
-              </span>
-            </div>
-          );
-        })
-      }
+      <section className='room_list-items'>
+        {
+          rooms !== undefined && rooms.map((roomID: string) => {
+            return (
+              <div className='room-list__item' key={roomID} onClick={() => {
+                history.push('/room/' + roomID);
+              }} title={'/room/' + roomID}>
+                <span className='room-list__item-content'>
+                  <div className='room-list__item-avatar'>
+                    {roomID[0]}
+                  </div>
+                  <p className='room-list__item-title'>{roomID}</p>
+                </span>
+              </div>
+            );
+          })
+        }
+      </section>
       <button className='room-list__create' onClick={() => history.push('/room/' + v4())}>
         +
       </button>
