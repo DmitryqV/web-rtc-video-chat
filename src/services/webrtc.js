722b6ccc28f440c4a79967a3ced75a755b27e36f
DmitryqV <vlasenko_dima@icloud.com>
2021-10-29 23:53:12 +0500

    refactoring code base

--- src/services/webrtc.js
+++ src/services/webrtc.js
@@ -107,8 +107,9 @@ export const webRTC = (roomID) => {
         };
       });
     };
-
+    // ====================================================
     // connect to switcher in user-access / user-access.tsx;
+    // ====================================================
     startRecord({ video: true, audio: true })
       .catch((e) => {
         console.warn(e);
