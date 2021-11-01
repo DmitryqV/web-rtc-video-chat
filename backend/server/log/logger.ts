59d63b417d2ee4a4232b05a0c29832fca4d36ed1
DmitryqV <vlasenko_dima@icloud.com>
2021-11-02 04:13:14 +0500

    refactoring server side

--- backend/server/log/logger.ts
+++ backend/server/log/logger.ts
@@ -4,24 +4,20 @@ const logger: Logger = log4js.getLogger();
 logger.level = 'info';
 
 log4js.configure(
-  (process.env.NODE_ENV === 'production') ? (
-    {
-      appenders: {
-        'console': { type: 'console' },
-        'file': { type: 'file', filename: 'server.log' }
-      },
-      categories: {
-        default: { appenders: ['file', 'console'], level: 'INFO' },
-      }
+  (process.env.NODE_ENV === 'production') ? ({
+    appenders: {
+      'console': { type: 'console' },
+      'file': { type: 'file', filename: 'server.log' }
+    },
+    categories: {
+      default: { appenders: ['file', 'console'], level: 'INFO' },
     }
-  ) : (
-    {
-      appenders: { 'console': { type: 'console' } },
-      categories: {
-        default: { appenders: ['console'], level: 'INFO' },
-      }
+  }) : ({
+    appenders: { 'console': { type: 'console' } },
+    categories: {
+      default: { appenders: ['console'], level: 'INFO' },
     }
-  )
+  })
 );
 
 export { logger };
