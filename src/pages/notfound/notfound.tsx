713ec67ba25b45046eab05ddb2a304e91c531337
DmitryqV <vlasenko_dima@icloud.com>
2021-11-02 00:14:16 +0500

    added not found page layout

--- src/pages/notfound/notfound.tsx
+++ src/pages/notfound/notfound.tsx
@@ -1,5 +1,16 @@
 import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
+import './notfound.css';
 
 export const NotFoundPage: FC = () => {
-  return <h5>Welcome to notfound page!</h5>;
+  return (
+    <section className='notfound-class'>
+      <h2 className='notfound-title'>page not found!</h2>
+      <Link to='/'>
+        <button className='notfound-btn'>
+          Go back
+        </button>
+      </Link>
+    </section>
+  );
 };
