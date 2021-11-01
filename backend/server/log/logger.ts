47f76ce9a6399bda4243b04b48160362d7f0ba0e
DmitryqV <vlasenko_dima@icloud.com>
2021-11-02 00:35:14 +0500

    change logger configure

--- backend/server/log/logger.ts
+++ backend/server/log/logger.ts
@@ -3,22 +3,28 @@ import log4js, { Logger } from "log4js";
 const logger: Logger = log4js.getLogger();
 logger.level = 'info';
 
-log4js.configure({
-  appenders: {
-    'console': {
-      type: 'console'
-    },
-    'file': {
-      type: 'file',
-      filename: 'server.log'
+
+log4js.configure(
+  (process.env.NODE_ENV === 'production') ? (
+    {
+      appenders: {
+        'console': { type: 'console' },
+        'file': { type: 'file', filename: 'server.log' }
+      },
+      categories: {
+        default: { appenders: ['file', 'console'], level: 'INFO' },
+      }
     }
-  },
-  categories: {
-    default: {
-      appenders: ['file', 'console'],
-      level: 'INFO'
-    },
-  }
-});
+  ) : (
+    {
+      appenders: { 'console': { type: 'console' } },
+      categories: {
+        default: { appenders: ['file'], level: 'INFO' },
+      }
+    }
+  )
+);
+
+
 
 export { logger };
