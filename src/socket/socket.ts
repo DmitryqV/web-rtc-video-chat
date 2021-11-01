dc4ea43ee921a28c205c76b40d2d6e3f5b2129bc
DmitryqV <vlasenko_dima@icloud.com>
2021-11-01 23:55:53 +0500

    refactoring code base

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -1,6 +1,6 @@
-import { io } from 'socket.io-client';
+import { io, Socket } from 'socket.io-client';
 
-export const socket = io(process.env.NODE_ENV === 'production' ? '/' : 'http://localhost:3001/', {
+export const socket: Socket = io(process.env.NODE_ENV === 'production' ? '/' : 'http://localhost:3001/', {
   forceNew: true,
   timeout: 1000,
   transports: ['websocket'],
