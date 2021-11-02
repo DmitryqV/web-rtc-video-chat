65023fa2862329daceccc694b3addd4ff8adb57b
DmitryqV <vlasenko_dima@icloud.com>
2021-11-02 06:07:45 +0500

    type custom callback service

---
+++ src/services/customCB.ts
@@ -0,0 +1,20 @@
+import { MutableRefObject, useCallback, useEffect, useRef, useState } from 'react';
+
+export const useCustomCB = (init: string) => {
+  const [state, setState] = useState(init);
+  const cbRef: MutableRefObject<Function | null> = useRef(null);
+
+  const update = useCallback((newState: Function | string, cb: Function): void => {
+    cbRef.current = cb;
+    setState((prev: Function | string) => typeof newState === 'function' ? newState(prev) : newState);
+  }, []);
+
+  useEffect(() => {
+    if (cbRef.current) {
+      cbRef.current(state);
+      cbRef.current = null;
+    };
+  }, [state]);
+
+  return [state, update];
+};
